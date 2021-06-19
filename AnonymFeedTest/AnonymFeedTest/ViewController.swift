cc22a5d7a6e4ce3080c6b82a1276aad6e84cf167
Pavel Mishagin <xstyle71@gmail.com>
2021-06-19 15:09:34 +0300

    init root

---
+++ AnonymFeedTest/AnonymFeedTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnonymFeedTest
+//
+//  Created by Павел Мишагин on 19.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
