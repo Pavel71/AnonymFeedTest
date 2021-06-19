810aa561bd1ea35b71e7ffececc975398cc90beb
Pavel Mishagin <xstyle71@gmail.com>
2021-06-19 15:47:59 +0300

    Configure Api Service

---
+++ AnonymFeedTest/AnonymFeedTest/Services/API/APIService.swift
@@ -0,0 +1,65 @@
+//
+//  APIService.swift
+//  AnonymFeedTest
+//
+//  Created by Павел Мишагин on 19.06.2021.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let feedItemsCount = 20
+}
+
+enum Endpoint {
+    
+    case first(_ count: Int)
+    case after(_ cursor: String)
+    case orderBy(_ filter: String)
+
+    
+    var baseURL:URL {URL(string: "https://k8s-stage.apianon.ru/posts/v1/posts")!}
+    
+    
+    var absoluteURL: URL? {
+        let queryURL = baseURL
+        let components = URLComponents(url: queryURL, resolvingAgainstBaseURL: true)
+        guard var urlComponents = components else {
+            return nil
+        }
+        
+        switch self {
+        case .first(let count):
+            urlComponents.queryItems = [URLQueryItem(name: "first", value: String(count))]
+        case .after(let cursor):
+            urlComponents.queryItems = [URLQueryItem(name: "after", value: cursor)]
+        case .orderBy(let filter):
+            urlComponents.queryItems = [URLQueryItem(name: "orderBy", value: filter)]
+        }
+
+        return urlComponents.url
+    }
+    
+}
+
+
+class APIService {
+    
+    func fetch<T: Codable>(from endPoint: Endpoint, completion: @escaping (T) -> ()) {
+
+        guard let url = endPoint.absoluteURL else { return }
+        print("URl",url)
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            do {
+                let object = try JSONDecoder().decode(T.self, from: data)
+                completion(object)
+            } catch let error as NSError {
+                print(error.userInfo)
+            }
+        }.resume()
+    }
+}
+
+    
+    
---
+++ AnonymFeedTest/AnonymFeedTest/Services/ServiceLocator.swift
@@ -0,0 +1,33 @@
+//
+//  ServiceLocator.swift
+//  AnonymFeedTest
+//
+//  Created by Павел Мишагин on 19.06.2021.
+//
+
+import Foundation
+
+protocol ServiceLocating {
+    func getService<T>() -> T?
+}
+
+final class ServiceLocator: ServiceLocating {
+  
+    public static let shared = ServiceLocator()
+    private lazy var services: [String: Any] = [:]
+
+    private func typeName(some: Any) -> String {
+        return (some is Any.Type) ?
+            "\(some)" : "\(type(of: some))"
+    }
+
+    func addService<T>(service: T) {
+        let key = typeName(some: T.self)
+        services[key] = service
+    }
+
+    func getService<T>() -> T? {
+        let key = typeName(some: T.self)
+        return services[key] as? T
+    }
+}
